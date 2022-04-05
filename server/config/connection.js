96c0defebf49bd13a366274b8dcf54eb6305d28c
CChampness <87551272+CChampness@users.noreply.github.com>
2022-04-05 19:57:44 -0400

    changed database name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/talk2me', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/moremessages', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
